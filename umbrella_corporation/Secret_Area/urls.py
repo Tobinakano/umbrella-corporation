f723e2aa1379808acb8e532e2268385fcac313b2
Andres Vargas <avargasm2@correo.usbcali.edu.co>
2026-04-09 20:27:08 -0500

    se agrega parte de Secret Area previa a una actualizacion en la landing

--- umbrella_corporation/Secret_Area/urls.py
+++ umbrella_corporation/Secret_Area/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'Secret_Area'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('landing/', views.landing, name='landing'),
+    path('empresa/', views.empresa, name='empresa'),
+    path('investigacion/', views.investigacion, name='investigacion'),
+    path('ubicaciones/', views.ubicaciones, name='ubicaciones'),
 ]
--- umbrella_corporation/Secret_Area/views.py
+++ umbrella_corporation/Secret_Area/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def landing(request):
+    return render(request, 'landing.html')
+
+def empresa(request):
+    return render(request, 'empresa.html')
+
+def investigacion(request):
+    return render(request, 'investigacion.html')
+
+def ubicaciones(request):
+    return render(request, 'ubicaciones.html')
