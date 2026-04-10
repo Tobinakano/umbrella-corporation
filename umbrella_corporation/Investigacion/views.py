50ee7af8b2e9f8c1d03d5bc730c81e2fea475459
Samuel Ceballos <daivor869@gmail.com>
2026-04-10 00:03:12 -0500

    feat: implement Investigacion landing page with styles and routing

--- umbrella_corporation/Investigacion/views.py
+++ umbrella_corporation/Investigacion/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'Investigacion/landing.html')
