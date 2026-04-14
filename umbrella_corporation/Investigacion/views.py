06792b3ed04217814c878332047d1cb2ce7bd870
Tobinakano <nicolashdezrdo@gmail.com>
2026-04-13 23:49:00 -0500

    Se limpio archivos repetidos y se organizo la logica para unirse a la devods

--- umbrella_corporation/Investigacion/views.py
+++ umbrella_corporation/Investigacion/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'Investigacion/landing.html')
+    return render(request, 'landing_investigacion.html')
